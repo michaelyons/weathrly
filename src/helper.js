7b8913176e725540151588b08559aaa80425a6c7
Haley Jacobs <haleyljacobs@gmail.com>
2018-07-19 16:27:54 -0600

    Added cleaner methods to helper.js

--- src/helper.js
+++ src/helper.js
@@ -12,3 +12,20 @@ const currentDayForecastObject = {
   icon: currentDayForecast.icon_url
 }
 
+
+const sevenHourForecast = data.hourly_forecast.filter(obj => Object.values(obj)[0]).reduce((sevenHour, hour, i) => {
+  if(i < 8){
+    sevenHour.push({ 
+      'time': hour.FCTTIME.civil, 
+      'temp': hour.temp.english + '°F', 
+      'condition': hour.condition, icon_url: hour.icon_url })
+  }
+  return sevenHour
+}, [])
+
+
+
+
+const tenDay = data.forecast.simpleforecast.forecastday.map(obj => {
+  return {day: obj.date.weekday, 'date': obj.date.month +'/'+ obj.date.day + '/' + obj.date.year , 'high': obj.high.fahrenheit + '°F', 'low': obj.low.fahrenheit + '°F', 'icon': obj.icon_url}
+})
