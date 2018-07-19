d64c4a58ef1006cef83901abb7a902c6cebeee2a
Michael J. Lyons <mikelyons7@me.com>
2018-07-18 18:25:21 -0600

    Add fetch call

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,17 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
-const CurrentWeather = props => {
+const CurrentWeather = ({ CurrentWeather }) => {
   return (
-    <div>
+    <div className="currentWeather">
       <header>Current Weather</header>
-      <h4>{props.currentWeather.local_time_rfc822}</h4>
-      <h5>{props.currentWeather.local_time_rfc822}</h5>
-      <h1>{props.currentWeather.temp_f}</h1>
-      <h3>{props.currentWeather.display_location.full}</h3>
-      <p>{props.currentWeather.weather}</p>
-      <h5>{props.highTemp}</h5>
-      <h5>{props.lowTemp}</h5>
-      <p>{props.dailyWeatherSummary}</p>
     </div>
   );
 };
