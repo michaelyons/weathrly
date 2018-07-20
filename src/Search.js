8bedb31504bfd5d1b99a29dc8a4d5a0cb8a4bdb1
Michael J. Lyons <mikelyons7@me.com>
2018-07-20 17:27:50 -0600

    Add placeholder text to input

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class Search extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       searchInput: ""
     };
@@ -12,11 +12,19 @@ class Search extends Component {
     return (
       <div>
         <input
+          type="text"
+          value={this.state.searchInput}
+          placeholder="Enter a City and State or Zip Code."
           onChange={event => {
             this.setState({ searchInput: event.target.value });
           }}
         />
-        <button onClick={event => this.props.dataFetch(this.state.searchInput)}>
+        <button
+          onClick={event => {
+            event.preventDefault();
+            this.props.dataFetch(this.state.searchInput);
+          }}
+        >
           ENTER
         </button>
       </div>
