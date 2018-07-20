cc054b1af974900267e64e8e0c139d6ba9b51f21
Haley Jacobs <haleyljacobs@gmail.com>
2018-07-20 17:26:04 -0600

    Edited CurrentWeather styling

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -4,15 +4,17 @@ import "./CurrentWeather.css";
 const CurrentWeather = props => {
   return (
     <div className="currentWeather">
-      <h2>{props.currWeather.time}</h2>
-      <h3>{props.currWeather.location}</h3>
-      <h2>{props.currWeather.current}</h2>
+      <h3 className="location">{props.currWeather.location}</h3>
+      <div className="left-side" >
+        <img src={props.currWeather.icon} className="weather-icon"/>
+        <p>{props.currWeather.conditions}</p>
       <p>
         {props.currWeather.high}
         {props.currWeather.low}
       </p>
-      <p>{props.currWeather.conditions}</p>
-      <p>{props.currWeather.icon}</p>
+      </div>
+      <h2 className="right-side">{props.currWeather.current}</h2>
+        {/*<h2>{props.currWeather.time}</h2>*/}
     </div>
   );
 };
