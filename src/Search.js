f0b1d79cef4b56c7f32edb7ed8f81feab82ae892
Haley Jacobs <haleyljacobs@gmail.com>
2018-07-21 16:28:45 -0600

    Added state search input

--- src/Search.js
+++ src/Search.js
@@ -12,20 +12,39 @@ class Search extends Component {
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.searchInput}
-          placeholder="Enter a City and State or Zip Code."
-          onChange={event => {
-            this.setState({ searchInput: event.target.value });
-          }}
-        />
-        <button
-          onClick={event => this.props.dataFetch(this.state.searchInput)}
-          className="search-button"
-        >
-          ENTER
-        </button>
+        <div className="first-input">
+          <input
+            type="text"
+            value={this.state.searchInput}
+            placeholder="enter a city or zip code"
+            onChange={event => {
+              this.setState({ searchInput: event.target.value });
+            }}
+          />
+          <button
+            onClick={event => this.props.dataFetch(this.state.searchInput)}
+            className="search-button"
+          >
+            ENTER
+          </button>
+        </div>
+        <div className="second-input">
+            <input
+            type="text"
+            value={this.state.searchInput}
+            placeholder="enter a state"
+            onChange={event => {
+              this.setState({ searchInput: event.target.value });
+            }}
+          />
+
+          <button
+            onClick={event => this.props.dataFetch(this.state.searchInput)}
+            className="search-button"
+          >
+            ENTER
+          </button>
+        </div>
       </div>
     );
   }
