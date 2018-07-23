b1a613e2556ea1bc61984ce9ab6376ccdf65fbc5
Michael J. Lyons <mikelyons7@me.com>
2018-07-23 17:07:22 -0600

    Fix :bug: in render

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
     if (localStorage.incomingWeather)
       this.locationChange(JSON.parse(localStorage.incomingWeather).location)
     }
-  }
+  
 
   render() {
     if (!this.state.location) {
