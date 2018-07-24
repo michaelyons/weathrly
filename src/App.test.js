217112f455e67d5f13d1a08d5f52355647054f4b
Michael J. Lyons <mikelyons7@me.com>
2018-07-24 07:30:31 -0600

    Add default state tests to app testing suite :white_check_mark:

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { configure, shallow, mount } from 'enzyme'
+import { configure , shallow, mount } from 'enzyme'
 import App from './App';
-// import localStorage from './setupTests'
+import localStorage from './setupTests'
 
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 describe('App', () => {
-    let shallowWrapper;
-    let mountWrapper;
+  let shallowWrapper;
+  let mountWrapper;
 
-beforeEach() => {
-  shallow =shallow(<App />)
-  mount = mount(<App />)
-}
+  beforeEach(() => {
+    shallowWrapper = shallow(<App />)
+    mountWrapper = mount(<App />)
+    console.log(shallowWrapper);
+  })
 
-}
+  it('should exist', () => {
+    expect(shallowWrapper).toBeDefined();
+    expect(mountWrapper).toBeDefined();
+  })
 
+  it('should have a default state containing an empty string for location', () => {
+    expect(shallowWrapper.state().location).toEqual('');
+  })
+
+  it('should have a default state containing an empty string for location', () => {
+    expect(shallowWrapper.state().location).toEqual('');
+  })
+
+  it('should have a default state containing an empty string for location', () => {
+    expect(shallowWrapper.state().location).toEqual('');
+  })
+
+  it('should render a Welcome and Search component on default', () => {
+    expect(shallowWrapper.find('WelcomePage').length).toEqual(1);
+    expect(shallowWrapper.find('Search').length).toEqual(1);
+  })
+
+  // it('should render a Search, Welcome, CurrentWeather, SevenHour, and TenDay components', () => {
+  //   shallowWrapper.instance().locationChange("poop");
+  //   expect(shallowWrapper.find('Search').length).toEqual(1);
+  //   expect(shallowWrapper.find('CurrentWeather').length).toEqual(1);
+  //   expect(shallowWrapper.find('SevenHour').length).toEqual(1);
+  //   expect(shallowWrapper.find('TenDay').length).toEqual(1);
+  // })
+});
