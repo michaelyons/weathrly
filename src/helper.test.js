63fe018477781f2dc327b5d1d63b76648842d384
Haley Jacobs <haleyljacobs@gmail.com>
2018-07-26 13:58:35 -0600

    Added seven hour helper test

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,7 +1,8 @@
 import { currWeather, sevenHour, tenDay } from "./helper.js";
 import data from "./MockData.js";
 
-describe("CurrentWeather", () => {
+describe("helper methods", () => {
+
   it("should take in data and filter to return the current weather object", () => {
     let helpData = currWeather(data);
     const date = new Date();
@@ -16,5 +17,43 @@ describe("CurrentWeather", () => {
       low: "32°F",
       time: "Last Updated " + realTime
     });
+  });
+
+  it("should take in data and filter to return the seven hour weather objects", () => {
+    let sevenHourData = sevenHour(data);
+    expect(sevenHourData).toEqual(
+    [ {  time: '12:00 PM',
+         temp: '47°F',
+         condition: 'Partly Cloudy',
+         icon_url: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' },
+       { time: '1:00 PM',
+         temp: '49°F',
+         condition: 'Partly Cloudy',
+         icon_url: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' },
+       { time: '2:00 PM',
+         temp: '49°F',
+         condition: 'Partly Cloudy',
+         icon_url: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' },
+       { time: '3:00 PM',
+         temp: '51°F',
+         condition: 'Clear',
+         icon_url: 'http://icons.wxug.com/i/c/k/clear.gif' },
+       { time: '4:00 PM',
+         temp: '50°F',
+         condition: 'Clear',
+         icon_url: 'http://icons.wxug.com/i/c/k/clear.gif' },
+       { time: '5:00 PM',
+         temp: '48°F',
+         condition: 'Clear',
+         icon_url: 'http://icons.wxug.com/i/c/k/clear.gif' },
+       { time: '6:00 PM',
+         temp: '45°F',
+         condition: 'Clear',
+         icon_url: 'http://icons.wxug.com/i/c/k/nt_clear.gif' },
+       { time: '7:00 PM',
+         temp: '43°F',
+         condition: 'Clear',
+         icon_url: 'http://icons.wxug.com/i/c/k/nt_clear.gif' } ]
+    );
   });
 });
